e93b867b0219f374fcf6dd04ebb7f9104c35a85c
helly25 <6420169+helly25@users.noreply.github.com>
2026-09-06 19:24:08 +0100

    Cover comparison guard boundaries
    Signed-off-by: helly25 <6420169+helly25@users.noreply.github.com>

--- mbo/proto/matchers_test.cc
+++ mbo/proto/matchers_test.cc
@@ -77,6 +77,20 @@ TEST(Matchers, Approximately) {
       kGetExplanation(Approximately(EqualsProto(R"pb(val: 0.9)pb"), 0.01), msg), HasSubstr("modified: val: 0.9 -> 1"));
 }
 
+TEST(Matchers, RejectsInvalidApproximateFractions) {
+  constexpr double kNegativeFraction = -0.1;
+  constexpr double kTooLargeFraction = 1.1;
+  EXPECT_DEATH(
+      (void)Approximately(EqualsProto(TestMessage{}), 0.0, kNegativeFraction),
+      "Fraction for Approximately must be >= 0.0 and < 1.0");
+  EXPECT_DEATH(
+      (void)Approximately(EqualsProto(TestMessage{}), 0.0, 1.0), "Fraction for Approximately must be >= 0.0 and < 1.0");
+  EXPECT_DEATH(
+      (void)Approximately(EqualsProto(), 0.0, kNegativeFraction), "Fraction for Relatively must be >= 0.0 and <= 1.0");
+  EXPECT_DEATH(
+      (void)Approximately(EqualsProto(), 0.0, kTooLargeFraction), "Fraction for Relatively must be >= 0.0 and <= 1.0");
+}
+
 TEST(Matchers, TreatingNaNsAsEqual) {
   const TestMessage msg = ParseTextProtoOrDie(R"pb(val: nan)pb");
   EXPECT_THAT(msg, TreatingNaNsAsEqual(EqualsProto(R"pb(val: nan)pb")));
